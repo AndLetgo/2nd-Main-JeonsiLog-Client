c9c708540ee21ee13e54d10813fcaccdfbdb1f60
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-11-27 19:29:58 +0900

    ISSUE-3 [docs] folder setting

--- JeonsiLog/app/src/main/java/com/example/jeonsilog/view/MainActivity.kt
+++ JeonsiLog/app/src/main/java/com/example/jeonsilog/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.jeonsilog
+package com.example.jeonsilog.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.jeonsilog.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
