afcaecb22eaa5465eb4902049013f43618fe25ec
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-11-28 20:28:53 +0900

    ISSUE-15 [feat] BottomNavigation 적용

--- JeonsiLog/app/src/main/java/com/example/jeonsilog/view/MainActivity.kt
+++ JeonsiLog/app/src/main/java/com/example/jeonsilog/view/MainActivity.kt
@@ -2,11 +2,39 @@ package com.example.jeonsilog.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
 import com.example.jeonsilog.R
+import com.example.jeonsilog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fl_main,HomeFragment()).commit()
+
+        binding.bnvMain.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.item_home->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_main,HomeFragment()).commit()
+                }
+                R.id.item_search->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_main,SearchFragment()).setReorderingAllowed(true).commitAllowingStateLoss()
+                }
+                R.id.item_photoCalendar->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_main,PhotoCalendarFragment()).commit()
+                }
+                R.id.item_notification->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_main,NotificationFragment()).commit()
+                }
+                R.id.item_mypage->{
+                    supportFragmentManager.beginTransaction().replace(R.id.fl_main,MyPageFragment()).commit()
+                }
+            }
+            true
+        }
     }
 }
