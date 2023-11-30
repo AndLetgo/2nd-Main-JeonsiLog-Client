f00b41c91c735661f3d9f318a0578ed4384f2808
moondev03 <moondev03@gmail.com>
2023-11-30 10:47:49 +0900

    ISSUE-7 [feat] Kakao SDK Setting

---
+++ JeonsiLog/app/src/main/java/com/example/jeonsilog/widget/utils/GlobalApplication.kt
@@ -0,0 +1,24 @@
+package com.example.jeonsilog.widget.utils
+
+import android.app.Application
+import android.util.Log
+import com.example.jeonsilog.BuildConfig
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
+
+
+class GlobalApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        getKeyHash()
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_KEY)
+    }
+
+
+    private fun getKeyHash(){
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("Hash", keyHash)
+    }
+}
