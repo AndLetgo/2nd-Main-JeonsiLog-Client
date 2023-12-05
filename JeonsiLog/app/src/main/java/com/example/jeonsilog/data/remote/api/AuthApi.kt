152aadbd3cfd2a3c4f2c6edf75e83e565d7c4faf
moondev03 <moondev03@gmail.com>
2023-12-05 23:53:48 +0900

    ISSUE-7 [etc] ImageApi -> AuthApi 변경

---
+++ JeonsiLog/app/src/main/java/com/example/jeonsilog/data/remote/api/AuthApi.kt
@@ -0,0 +1,30 @@
+package com.example.jeonsilog.data.remote.api
+
+import com.example.jeonsilog.data.remote.dto.auth.IsAvailableResponse
+import com.example.jeonsilog.data.remote.dto.auth.LoginData
+import com.example.jeonsilog.data.remote.dto.auth.TokenResponse
+import com.example.jeonsilog.data.remote.dto.auth.ReissuedData
+import com.example.jeonsilog.data.remote.dto.auth.SignUpData
+import com.example.jeonsilog.data.remote.dto.auth.OnlyMsgResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface AuthApi {
+    @POST("/auth/sign-up")
+    suspend fun postUser(@Body userData: SignUpData): Response<OnlyMsgResponse>
+
+    @GET("/auth/nickname/{nick}")
+    suspend fun getIsAvailable(@Path("nick") nick: String): Response<IsAvailableResponse>
+
+    @POST("/auth/sign-in")
+    suspend fun postLogin(@Body loginData: LoginData): Response<TokenResponse>
+
+    @POST("/auth/refresh")
+    suspend fun postReissue(@Body reissueData: ReissuedData): Response<TokenResponse>
+//
+//    @POST("/auth/sign-out")
+//    suspend fun postLogOut(@Body)
+}
