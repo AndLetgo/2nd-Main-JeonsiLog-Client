781c47cc7e24ec25b28c519a7092f47ff0623786
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-12-09 13:25:31 +0900

    ISSUE-17 [feat] create fragment

---
+++ JeonsiLog/app/src/main/java/com/example/jeonsilog/view/exhibition/ExhibitionFragment.kt
@@ -0,0 +1,17 @@
+package com.example.jeonsilog.view.exhibition
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.jeonsilog.R
+import com.example.jeonsilog.base.BaseFragment
+import com.example.jeonsilog.databinding.FragmentExhibitionBinding
+
+class ExhibitionFragment : BaseFragment<FragmentExhibitionBinding>(R.layout.fragment_exhibition) {
+    override fun init() {
+        
+    }
+
+}
