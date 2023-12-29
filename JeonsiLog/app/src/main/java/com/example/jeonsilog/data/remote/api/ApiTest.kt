06d7e33080b656a2d5f63c221887ff53eba3001a
moondev03 <moondev03@gmail.com>
2023-12-29 16:47:23 +0900

    ISSUE-26 [feat] Rating API - 별점 제거 기능 제외

--- JeonsiLog/app/src/main/java/com/example/jeonsilog/data/remote/api/ApiTest.kt
+++ JeonsiLog/app/src/main/java/com/example/jeonsilog/data/remote/api/ApiTest.kt
@@ -1,8 +1,6 @@
 package com.example.jeonsilog.data.remote.api
 
-import com.example.jeonsilog.data.remote.dto.exhibition.PatchExhibitionRequest
-import com.example.jeonsilog.data.remote.dto.exhibition.UpdatePlaceInfoEntity
-import com.example.jeonsilog.repository.exhibition.ExhibitionRepositoryImpl
+import com.example.jeonsilog.repository.rating.RatingRepositoryImpl
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -10,27 +8,13 @@ import kotlinx.coroutines.launch
 class ApiTest {
     // android1 로 로그인
     private val token =
-        "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI3IiwiaWF0IjoxNzAzNzg4NDQ5LCJleHAiOjE3MDM3OTIwNDl9.zVsWq4l-9F-z_f3DkGXi4r8Ra32byvjciIrMsF5KIf_Ige1WomuolYYMdMnJcmoQQiBslsrkrCtLmD2Riu5DZA"
+        "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI3IiwiaWF0IjoxNzAzODMzNzYxLCJleHAiOjE3MDM4MzczNjF9.GWArY1HojFVGAZogIG6QcIj4YUCL2botx4w3O42hbuwPsCkvqmx-Gc69soTXEWvjw0omBtnYARhHfKd52KcSSQ"
+
 
     init {
         CoroutineScope(Dispatchers.IO).launch {
-            val body = PatchExhibitionRequest(
-                1,
-                "한국필묵그룹 선흔 창립20주년 기념전",
-                "ON_DISPLAY",
-                "FREE",
-                "이 전시회는 어떤 전시회입니다.",
-                UpdatePlaceInfoEntity(
-                    1,
-                    "서울특별시 송파구 올림픽로 300 롯데월드몰 8층 롯데콘서트홀",
-                    "~~~ ~~~",
-                    "MONDAY",
-                    "031-590-4358",
-                    "\thttps://culture.nyj.go.kr/home/"
-                )
-            )
-
-            ExhibitionRepositoryImpl().patchExhibition(token, body)
+            RatingRepositoryImpl().deleteRating(token, 1)
+            RatingRepositoryImpl().getMyRatings(token)
         }
     }
 }
