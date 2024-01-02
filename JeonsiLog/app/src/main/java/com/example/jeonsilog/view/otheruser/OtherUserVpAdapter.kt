922772caf2ee5d5cfc7f71af18f2e9ca31f15167
moondev03 <moondev03@gmail.com>
2024-01-02 20:52:30 +0900

    ISSUE-32 [feat] 타 유저 페이지 - 포토캘린더

--- JeonsiLog/app/src/main/java/com/example/jeonsilog/view/otheruser/OtherUserVpAdapter.kt
+++ JeonsiLog/app/src/main/java/com/example/jeonsilog/view/otheruser/OtherUserVpAdapter.kt
@@ -12,7 +12,7 @@ class OtherUserVpAdapter(fragmentActivity: FragmentActivity, private val vm: Oth
         return when(position) {
             0 -> OtherUserRatingFragment(vm, otherUserId)
             1 -> OtherUserReviewFragment(vm, otherUserId)
-            2 -> OtherUserCalendarFragment()
+            2 -> OtherUserCalendarFragment(otherUserId)
             else -> throw IllegalArgumentException("유효하지 않은 인덱스 번호: $position")
         }
     }
