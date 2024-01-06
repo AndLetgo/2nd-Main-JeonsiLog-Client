ba9c296a338b21dc12f9f98442c596eb9ccfcdb3
syjeuion <105288129+syjeuion@users.noreply.github.com>
2024-01-06 21:31:22 +0900

    ISSUE-30 [mod] pull from develop

--- JeonsiLog/app/src/main/java/com/example/jeonsilog/view/search/UserSearchItemAdapter.kt
+++ JeonsiLog/app/src/main/java/com/example/jeonsilog/view/search/UserSearchItemAdapter.kt
@@ -38,12 +38,12 @@ class UserSearchItemAdapter(private val context: Context,private val edittext:St
         val item = list[position]
         //list.size가 position 보다 커야함
         //list의 마지막요소가 아닌 마지막 전요소에서 비교
-        if (list.size>5){
-            if (list.size-4==position){
-                itemPage+=1
+        if (list.size > 5) {
+            if (list.size - 4 == position) {
+                itemPage += 1
                 runBlocking(Dispatchers.IO) {
-                    val response = UserRepositoryImpl().searchUserInfo(encryptedPrefs.getAT(),edittext)
-                    if(response.isSuccessful && response.body()!!.check){
+                    val response = UserRepositoryImpl().searchUserInfo(encryptedPrefs.getAT(), edittext)
+                    if (response.isSuccessful && response.body()!!.check) {
                         val searchUserResponse = response.body()
                         list.addAll(searchUserResponse?.informationEntity!!.toMutableList())
                     }
@@ -59,11 +59,12 @@ class UserSearchItemAdapter(private val context: Context,private val edittext:St
         holder.itemView.setOnClickListener {
             //유저 id
 
-        holder.itemView.setOnClickListener {
-            (context as MainActivity).moveOtherUserProfile(item.userId, item.nickname)
+            holder.itemView.setOnClickListener {
+                (context as MainActivity).moveOtherUserProfile(item.userId, item.nickname)
+
+            }
 
         }
-
     }
 
     override fun getItemCount(): Int {
