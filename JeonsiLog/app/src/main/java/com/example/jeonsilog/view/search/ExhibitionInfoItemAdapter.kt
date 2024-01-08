7ba3dbf7649457037e7add11622be63549c89bec
Liminghing <87886235+Liminghing@users.noreply.github.com>
2024-01-08 19:53:15 +0900

    ISSUE-45 [feat] 특수문자 처리 기능

--- JeonsiLog/app/src/main/java/com/example/jeonsilog/view/search/ExhibitionInfoItemAdapter.kt
+++ JeonsiLog/app/src/main/java/com/example/jeonsilog/view/search/ExhibitionInfoItemAdapter.kt
@@ -7,8 +7,11 @@ import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import androidx.core.view.marginStart
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -18,6 +21,7 @@ import com.example.jeonsilog.R
 import com.example.jeonsilog.data.remote.dto.ExhibitionInfoItem
 import com.example.jeonsilog.data.remote.dto.exhibition.SearchInformationEntity
 import com.example.jeonsilog.data.remote.dto.exhibition.SearchResponse
+import com.example.jeonsilog.databinding.ItemExhibitionInfoBinding
 import com.example.jeonsilog.repository.exhibition.ExhibitionRepositoryImpl
 import com.example.jeonsilog.view.MainActivity
 import com.example.jeonsilog.widget.utils.GlideApp
@@ -28,22 +32,25 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import org.jsoup.Jsoup
 
 class ExhibitionInfoItemAdapter(private val context: Context,private val edittext:String, private val list:MutableList <SearchInformationEntity>) : RecyclerView.Adapter<ExhibitionInfoItemAdapter.ViewHolder>() {
     var itemPage=0
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val exhibitionnameTextView: TextView = view.findViewById(R.id.tv_title)
-        val exhibitionlocationTextView: TextView = view.findViewById(R.id.tv_address)
-        val exhibitionplaceTextView: TextView = view.findViewById(R.id.tv_place)
-        val exhibitiondateTextViewBefore: TextView = view.findViewById(R.id.tv_keyword_operating_before)
-        val exhibitiondateTextViewIng: TextView = view.findViewById(R.id.tv_keyword_operating_ing)
-        val exhibitionpriceTextViewFree: TextView = view.findViewById(R.id.tv_keyword_price_free)
-
+    class ViewHolder(private val binding: ItemExhibitionInfoBinding) : RecyclerView.ViewHolder(binding.root) {
+        val exhibitionnameTextView: TextView = binding.tvTitle
+        val exhibitionlocationTextView: TextView = binding.tvAddress
+        val exhibitionplaceTextView: TextView = binding.tvPlace
+        val exhibitiondateTextViewBefore: TextView = binding.tvKeywordOperatingBefore
+        val exhibitiondateTextViewIng: TextView = binding.tvKeywordOperatingIng
+        val exhibitionpriceTextViewFree: TextView = binding.tvKeywordPriceFree
+        val posterImageView: ImageView = binding.ivPoster
+        val marginFirst=binding.sMarginFirst
+        val marginSecond=binding.sMarginSecond
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_exhibition_info, parent, false)
-        return ViewHolder(view)
+        val binding = ItemExhibitionInfoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -69,8 +76,14 @@ class ExhibitionInfoItemAdapter(private val context: Context,private val edittex
         GlideApp.with(context)
             .load(unsplashUrl)
             .transform(CenterCrop(), RoundedCorners(radiusPx))
-            .into(holder.itemView.findViewById(R.id.iv_poster))
-        holder.exhibitionnameTextView.text =item.exhibitionName
+            .into(holder.posterImageView)
+
+
+        //특수문자 처리
+        val decodedString = Jsoup.parse(item.exhibitionName).text()
+        holder.exhibitionnameTextView.text =decodedString
+
+
         if (item.place.placeAddress!=null){
             val words = item.place.placeAddress.split(" ")
             // 앞의 2개의 단어 추출
@@ -81,26 +94,43 @@ class ExhibitionInfoItemAdapter(private val context: Context,private val edittex
             }
         }
         else{
-            holder.exhibitionlocationTextView.isGone=true
+            holder.exhibitionlocationTextView.text=""
+            //holder.exhibitionlocationTextView.isVisible=false
+        }
+        if (item.place.placeName!=null){
+            holder.exhibitionplaceTextView.text = item.place.placeName
+        }
+        else{
+            holder.exhibitionplaceTextView.text=""
+            //holder.exhibitionplaceTextView.isVisible=false
         }
 
-        holder.exhibitionplaceTextView.text = item.place.placeName
 
 
         if (item.operatingKeyword=="AFTER_DISPLAY"){
-            holder.exhibitiondateTextViewBefore.isGone=true
-            holder.exhibitiondateTextViewIng.isGone=true
-            Log.d("TAG", "$item.operatingKeyword: ")
+            //
+            holder.exhibitiondateTextViewBefore.isGone=true //시작전 키워드 없음
+            holder.exhibitiondateTextViewIng.isGone=true //전시중 키워드 없음
+            holder.marginFirst.isGone=true//시작전 키워드와 전시중 키워드 사이 마진 제거
         }else if (item.operatingKeyword=="ON_DISPLAY"){
-            holder.exhibitiondateTextViewBefore.isGone=false
-            holder.exhibitiondateTextViewIng.isGone=true
-            Log.d("TAG", "$item.operatingKeyword: ")
+            //시작전
+            holder.exhibitiondateTextViewBefore.isGone=false //시작전 키워드 있음
+            holder.exhibitiondateTextViewIng.isGone=true //전시중 키워드 없음
+            holder.marginFirst.isGone=false//시작전 키워드와 전시중 키워드 사이 마진
+
         }else{
-            holder.exhibitiondateTextViewBefore.isGone=true
-            holder.exhibitiondateTextViewIng.isGone=false
-            Log.d("TAG", "$item.operatingKeyword: ")
+            //전시중
+            holder.exhibitiondateTextViewBefore.isGone=true //시작전 키워드 없음
+            holder.exhibitiondateTextViewIng.isGone=false //전시중 키워드 있음 => 마진값 변경
+            holder.marginFirst.isGone=false//시작전 키워드와 전시중 키워드 사이 마진
         }
-        holder.exhibitionpriceTextViewFree.isGone = item.priceKeyword=="PAY"
+        if (item.priceKeyword=="PAY") {
+            holder.marginSecond.isGone=false
+            holder.exhibitionpriceTextViewFree.isGone = true
+        }else{
+            holder.marginSecond.isGone=true
+            holder.exhibitionpriceTextViewFree.isGone = false
+        }
         holder.itemView.setOnClickListener {
             //@@클릭처리
             //전시회 id
