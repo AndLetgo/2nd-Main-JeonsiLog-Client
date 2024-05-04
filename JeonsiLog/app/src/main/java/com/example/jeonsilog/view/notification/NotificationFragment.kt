5ba664670a464b13f07e6c566165e28c433d23ef
Liminghing <87886235+Liminghing@users.noreply.github.com>
2024-05-04 22:01:14 +0900

    ISSUE-85 [feat] AdMob 구현

--- JeonsiLog/app/src/main/java/com/example/jeonsilog/view/notification/NotificationFragment.kt
+++ JeonsiLog/app/src/main/java/com/example/jeonsilog/view/notification/NotificationFragment.kt
@@ -6,6 +6,10 @@ import com.example.jeonsilog.R
 import com.example.jeonsilog.base.BaseFragment
 import com.example.jeonsilog.databinding.FragmentNotificationBinding
 import com.example.jeonsilog.view.MainActivity
+import com.google.android.ads.nativetemplates.TemplateView
+import com.google.android.gms.ads.AdLoader
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import com.google.android.material.tabs.TabLayoutMediator
 
 class NotificationFragment(private val action:String) : BaseFragment<FragmentNotificationBinding>(
@@ -15,6 +19,17 @@ class NotificationFragment(private val action:String) : BaseFragment<FragmentNot
         (context as MainActivity).setStateBn(true, "user")
 
         val tabTextList = listOf(getString(R.string.noti_activity_tab), getString(R.string.noti_exhibition_tab))
+// Kotlin 파일의 클래스 내에서 아래 코드를 사용하세요.
+        MobileAds.initialize(requireActivity())
+        val adLoader = AdLoader.Builder(requireActivity(), "ca-app-pub-3940256099942544/2247696110")
+            .forNativeAd { nativeAd ->
+                binding.natSmallTemplate.setNativeAd(nativeAd)
+
+            }
+            .build()
+
+        adLoader.loadAd(AdRequest.Builder().build())
+
 
 
         if (action=="your_special_exhibition"){
