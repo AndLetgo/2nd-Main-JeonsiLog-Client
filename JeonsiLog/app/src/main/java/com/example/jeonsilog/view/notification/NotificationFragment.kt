765219933683c55b833800a4b5083b628be6b97c
Liminghing <87886235+Liminghing@users.noreply.github.com>
2024-05-08 20:30:27 +0900

    ISSUE-85 [feat] 광고로드 확인용 로그 작성

--- JeonsiLog/app/src/main/java/com/example/jeonsilog/view/notification/NotificationFragment.kt
+++ JeonsiLog/app/src/main/java/com/example/jeonsilog/view/notification/NotificationFragment.kt
@@ -2,13 +2,16 @@ package com.example.jeonsilog.view.notification
 
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import com.example.jeonsilog.R
 import com.example.jeonsilog.base.BaseFragment
 import com.example.jeonsilog.databinding.FragmentNotificationBinding
 import com.example.jeonsilog.view.MainActivity
 import com.google.android.ads.nativetemplates.TemplateView
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdLoader
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -19,13 +22,27 @@ class NotificationFragment(private val action:String) : BaseFragment<FragmentNot
         (context as MainActivity).setStateBn(true, "user")
 
         val tabTextList = listOf(getString(R.string.noti_activity_tab), getString(R.string.noti_exhibition_tab))
-// Kotlin 파일의 클래스 내에서 아래 코드를 사용하세요.
+        //테스트용 광고 ID : ca-app-pub-3940256099942544/2247696110
         MobileAds.initialize(requireActivity())
+//        val adLoader = AdLoader.Builder(requireActivity(), "ca-app-pub-3940256099942544/2247696110")
+//            .forNativeAd { nativeAd ->
+//                binding.natSmallTemplate.setNativeAd(nativeAd)
+//
+//            }
+//            .build()
+//
+//        adLoader.loadAd(AdRequest.Builder().build())
         val adLoader = AdLoader.Builder(requireActivity(), "ca-app-pub-3940256099942544/2247696110")
             .forNativeAd { nativeAd ->
                 binding.natSmallTemplate.setNativeAd(nativeAd)
-
+                Log.d("AdLoad", "광고가 성공적으로 로드되었습니다.")
             }
+            .withAdListener(object : AdListener() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    super.onAdFailedToLoad(adError)
+                    Log.d("AdLoad", "광고 로드 실패: ${adError.message}")
+                }
+            })
             .build()
 
         adLoader.loadAd(AdRequest.Builder().build())
